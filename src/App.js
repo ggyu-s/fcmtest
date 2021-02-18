b0c67ba5e89ea30694d3a7196257497834f8cc34
GyucheolJung <ggyu@GyucheolJungui-iMac.local>
2021-02-18 18:56:44 +0900

    fmc

--- src/App.js
+++ src/App.js
@@ -13,13 +13,21 @@ function App() {
   //     .catch((err) => console.log(err));
   // }, []);
   const onClick = () => {
-    messaging
-      .getToken({
-        vapidKey:
-          "BJ3iQP9GVNMys8sYfNJ5Agkf1iYX5sv2yUVyXlqrkBJMqWTfinFfp6nKRT2Ww9fzSycmAXgHjVY1ArgeXqYi-pM",
-      })
-      .then((res) => console.log(res))
-      .catch((err) => console.log(err));
+    if (Notification.permission !== "denied") {
+      Notification.requestPermission((permission) => {
+        if (permission === "granted") {
+          messaging
+            .getToken({
+              vapidKey:
+                "BJ3iQP9GVNMys8sYfNJ5Agkf1iYX5sv2yUVyXlqrkBJMqWTfinFfp6nKRT2Ww9fzSycmAXgHjVY1ArgeXqYi-pM",
+            })
+            .then((res) => console.log(res))
+            .catch((err) => console.log(err));
+        } else {
+          alert("Notification denied");
+        }
+      });
+    }
   };
 
   return <div onClick={onClick}>Firebase</div>;
--- src/fbase.js
+++ src/fbase.js
@@ -2,13 +2,13 @@ import firebase from "firebase/app";
 import "firebase/messaging";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDqgjAA1aQagEQ1nMx1eDm7GBvMF6pJ82w",
-  authDomain: "nwitter-61071.firebaseapp.com",
-  projectId: "nwitter-61071",
-  storageBucket: "nwitter-61071.appspot.com",
-  messagingSenderId: "653801352300",
-  appId: "1:653801352300:web:55c007104aa081874ebc49",
-  measurementId: "G-YM9N6QX9K2",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STOREAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 
 firebase.initializeApp(firebaseConfig);
