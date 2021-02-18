7c99215a16287af2a6363a48b1c810e2919fb533
GyucheolJung <ggyu@GyucheolJungui-iMac.local>
2021-02-19 07:48:47 +0900

    fmc

--- src/App.js
+++ src/App.js
@@ -2,33 +2,34 @@
 import React from "react";
 import { messaging } from "./fbase";
 
-// useEffect(() => {
-//   messaging.requestPermission();
-//   messaging
-//     .getToken({
-//       vapidKey:
-//         "BJ3iQP9GVNMys8sYfNJ5Agkf1iYX5sv2yUVyXlqrkBJMqWTfinFfp6nKRT2Ww9fzSycmAXgHjVY1ArgeXqYi-pM",
-//     })
-//     .then((res) => console.log(res))
-//     .catch((err) => console.log(err));
-// }, []);
-const onClick = () => {
-  if (Notification.permission !== "denied") {
-    Notification.requestPermission((permission) => {
-      if (permission === "granted") {
-        messaging
-          .getToken({
-            vapidKey:
-              "BJ3iQP9GVNMys8sYfNJ5Agkf1iYX5sv2yUVyXlqrkBJMqWTfinFfp6nKRT2Ww9fzSycmAXgHjVY1ArgeXqYi-pM",
-          })
-          .then((res) => console.log(res))
-          .catch((err) => console.log(err));
-      } else {
-        alert("Notification denied");
-      }
-    });
-  }
-
+const App = () => {
+  // useEffect(() => {
+  //   messaging.requestPermission();
+  //   messaging
+  //     .getToken({
+  //       vapidKey:
+  //         "BJ3iQP9GVNMys8sYfNJ5Agkf1iYX5sv2yUVyXlqrkBJMqWTfinFfp6nKRT2Ww9fzSycmAXgHjVY1ArgeXqYi-pM",
+  //     })
+  //     .then((res) => console.log(res))
+  //     .catch((err) => console.log(err));
+  // }, []);
+  const onClick = () => {
+    if (Notification.permission !== "denied") {
+      Notification.requestPermission((permission) => {
+        if (permission === "granted") {
+          messaging
+            .getToken({
+              vapidKey:
+                "BJ3iQP9GVNMys8sYfNJ5Agkf1iYX5sv2yUVyXlqrkBJMqWTfinFfp6nKRT2Ww9fzSycmAXgHjVY1ArgeXqYi-pM",
+            })
+            .then((res) => console.log(res))
+            .catch((err) => console.log(err));
+        } else {
+          alert("Notification denied");
+        }
+      });
+    }
+  };
   return <div onClick={onClick}>Firebase</div>;
 };
 
